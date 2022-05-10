ee54a3053a0651f7a7732ae8ee3c916783006c10
steffanorondonuwu <79073508+steffanorondonuwu@users.noreply.github.com>
2022-05-10 20:46:26 +0800

    getAlluser/getuser

--- app/chats/router.js
+++ app/chats/router.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    getAllUsers, getUser
-} = require('./controller');
-router.get('/user', getAllUsers);
-router.get('/user', getUser);
-
-module.exports = router;
-
-
-const { 
+    getAllUsers, 
+    getUser,
     deleteContact,
     deleteChat
 } = require('./controller');
+router.get('/users', getAllUsers);
+router.get('/user', getUser);
 router.delete('/user/deleteContact', deleteContact);
 router.delete('/user/clearChat', deleteChat);
 
-module.exports = router;
+module.exports = {
+    getAllUsers,
+    getUser,
+    deleteContact,
+    deleteChat,
+};
